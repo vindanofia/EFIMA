f22685b29b26036d395993e484ca7b640f264a29
vindanofia <vindanofia14@gmail.com>
2020-05-27 09:01:04 +0700

    file laporin

---
+++ application/controllers/Auth.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+	public function login()
+	{
+		check_already_login();
+		$this->load->view('login');
+	}
+
+	public function process()
+	{
+		$post = $this->input->post(null, TRUE);
+		if (isset($post['login'])) {
+			$this->load->model('m_user');
+			$query = $this->m_user->login($post);
+			if ($query->num_rows() > 0) {
+				$row = $query->row();
+				$params = array(
+					'userid' => $row->id_user,
+					'level' => $row->id_role
+				);
+				$this->session->set_userdata($params);
+				echo "<script>
+				alert('Selamat, login berhasil');
+				window.location='" . site_url('Admin/dashboard') . "';
+				</script>";
+			} else {
+				echo "<script>
+				alert('Login gagal');
+				window.location='" . site_url('auth/login') . "';
+				</script>";
+			}
+		}
+	}
+
+	public function logout()
+	{
+		$params = array('userid', 'level');
+		$this->session->unset_userdata($params);
+		redirect('auth/login');
+	}
+}
